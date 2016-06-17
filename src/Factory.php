2aa9d6851ed2ebee6845c152444d12afcbf5cf8f
Fabricio Valadares <valadares.fabricio@gmail.com>
2016-06-17 15:52:49 -0300

    Update

--- src/Factory.php
+++ src/Factory.php
@@ -7,7 +7,6 @@
  * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-
 namespace Cityware\Form;
 
 abstract class Factory
